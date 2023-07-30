b0bd16a18e71edb074164a1cc7f0e340cc5c249b
Binh Trinh <binhtrinh@Binhs-MacBook-Pro.local>
2023-07-30 23:09:27 +0700

    add: todo and some ideals

--- main.py
+++ main.py
@@ -16,10 +16,12 @@ processor.process_notes()
 processor.convert_book()
 
 # construct the notion and create the page
-notion = Notion(books=processor.books, secret=secret, database=database, url=url)
-notion.convert_books_to_notion_inputs()
-notion.prepare_header()
-notion.create_pages()
+# notion = Notion(books=processor.books, secret=secret, database=database, url=url)
+# notion.convert_books_to_notion_inputs()
+# notion.prepare_header()
+# notion.create_pages()
 # query and save the pages information into the file
-pages_infor_path = 'pages.json'
-notion.get_pages(pages_infor_path=pages_infor_path)
+# pages_infor_path = 'pages.json'
+# notion.get_pages(pages_infor_path=pages_infor_path)
+
+processor.collect_highlight() >> processor.process_notes()
