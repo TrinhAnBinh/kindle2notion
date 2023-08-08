f099be300a69c2fad90ec8497d7be500e608bb8e
Binh Trinh <binhtrinh@Binhs-MacBook-Pro.local>
2023-08-08 08:37:19 +0700

    feat: add checkpoint to filter input data BREAKING_CHANGE: added checkpoint to processor and notion, missing mode:update and create pages

--- main.py
+++ main.py
@@ -16,8 +16,36 @@ processor.collect_highlight()
 processor.process_notes()
 processor.convert_book()
 
+# construct checkpoint
+check = Checkpoint('checkpoint.json')
+
+# load checkpoint
+checkpoint = check.load()
+books = processor.books
+# filter highlight base on the checkpoint
+updated_books = []
+for book in books:
+    for cp in checkpoint:
+        if book['book_name'] == cp['book_name']: # old book
+            max_position = cp['block_offset']
+            if max_position < len(book['note']): # book was added new notes
+                book['note'] = book['note'][max_position:] 
+                book['location'] = book['location'][max_position:] 
+            else:
+                book['note'] = []
+                book['location'] = []
+    updated_books.append(book)
+
+# todo : should process the book in one step when create the updated_books
+books_input = []
+for ubook in updated_books:
+    if len(ubook['note']) > 0:
+        books_input.append(ubook)
+
+# todo create the mode :update book or created book
+
 # construct the notion and create the page
-notion = Notion(books=processor.books, secret=secret, database=database, url=url)
+notion = Notion(books=books_input, secret=secret, database=database, url=url)
 notion.convert_books_to_notion_inputs()
 notion.prepare_header()
 check_point = notion.create_pages()
@@ -25,12 +53,10 @@ check_point = notion.create_pages()
 pages_infor_path = 'pages.json'
 notion.get_pages(pages_infor_path=pages_infor_path)
 
-# construct checkpoint
 
-check = Checkpoint('checkpoint.json')
-# load check point data
+# update check point data
 updated_page_infor = check.construct_checkpoint(check_point)
+# save checkpoint to file
 check.save(updated_page_infor)
-
 
 # processor.collect_highlight() >> processor.process_notes()
--- notion.py
+++ notion.py
@@ -41,70 +41,71 @@ class Notion:
             data_inputs = []
             # data_updates = []
             for j in self.books:
-                children = []
-                for k in range(len(j['note'])):
-                    header = {
-                                "object": "block",
-                                "type": "heading_3",
-                                "heading_3": {
-                                    "rich_text": [{ "type": "text", "text": { "content": j['location'][k] } }]
+                if len(j['note']) > 0: # filtering book have more than one note
+                    children = []
+                    for k in range(len(j['note'])):
+                        header = {
+                                    "object": "block",
+                                    "type": "heading_3",
+                                    "heading_3": {
+                                        "rich_text": [{ "type": "text", "text": { "content": j['location'][k] } }]
+                                    }
                                 }
-                            }
-                    children.append(header)
-                    para = {
-                                "object": "block",
-                                "type": "paragraph",
-                                "paragraph": {
-                                    "rich_text": [
-                                        {
-                                            "type": "text",
-                                            "text": {
-                                                "content": j['note'][k],
+                        children.append(header)
+                        para = {
+                                    "object": "block",
+                                    "type": "paragraph",
+                                    "paragraph": {
+                                        "rich_text": [
+                                            {
+                                                "type": "text",
+                                                "text": {
+                                                    "content": j['note'][k],
+                                                }
                                             }
-                                        }
-                                    ]
+                                        ]
+                                    }
                                 }
-                            }
-                    children.append(para)
+                        children.append(para)
 
-                # Data input
-                data_input = {
-                    "parent": { "database_id": self.database },
-                    'icon': {
-                        'type': "emoji",
-                            'emoji': "🥬"
-                    },
-                    'cover': {
-                        'type': "external",
-                        'external': {
-                            'url': "https://upload.wikimedia.org/wikipedia/commons/6/62/Tuscankale.jpg"
-                        }
-                    },
-                    "properties": {
-                        'Author': {
-                            'id': 'KDEZ',
-                            'type': 'rich_text',
-                            'rich_text': [
+                    # Data input
+                    data_input = {
+                        "parent": { "database_id": self.database },
+                        'icon': {
+                            'type': "emoji",
+                                'emoji': "🥬"
+                        },
+                        'cover': {
+                            'type': "external",
+                            'external': {
+                                'url': "https://upload.wikimedia.org/wikipedia/commons/6/62/Tuscankale.jpg"
+                            }
+                        },
+                        "properties": {
+                            'Author': {
+                                'id': 'KDEZ',
+                                'type': 'rich_text',
+                                'rich_text': [
+                                    {
+                                        "text": {
+                                        "content": j['author']
+                                        }
+                                    },
+                                ]
+                        },
+                            "title": {
+                                "title": [
                                 {
                                     "text": {
-                                    "content": j['author']
+                                    "content": j['book_name']
                                     }
                                 },
-                            ]
-                    },
-                        "title": {
-                            "title": [
-                            {
-                                "text": {
-                                "content": j['book_name']
-                                }
-                            },
-                            ]
+                                ]
+                            }
                         }
+                        ,"children": children # todo need to create a function to update the page, because when creating a page, Notion only accept 100 blocks for children
                     }
-                    ,"children": children # todo need to create a function to update the page, because when creating a page, Notion only accept 100 blocks for children
-                }
-                data_inputs.append(data_input)
+                    data_inputs.append(data_input)
             self.notion_data_input =  data_inputs
         else:
             BaseException('Books notes have no data, rerun the pipeline')
